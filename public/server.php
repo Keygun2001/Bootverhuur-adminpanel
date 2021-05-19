01677f2beef7d499a40716c631aa0c03d973a8de
Keygun2001 <keygun2001@gmail.com>
2021-05-19 14:36:10 +0200

    Bestellingen fix

--- public/server.php
+++ public/server.php
@@ -22,7 +22,7 @@
         $dagdeelid = $_POST['dagdeelSelect'];
         $datum = $_POST['datum'];
         $luxe = $_POST['luxeSelect'];
-        db_insertData("INSERT INTO Verhuur (FirstName, LastName, PhoneNumber, Email, Password, BootID, DagID, DateVerhuur, LuxeID) VALUES ('$vnaam','$anaam', '$telnummer', '$email', '$ww', '$bootid', '$dagdeelid', '$datum', '$luxe')");
+        db_insertData("INSERT INTO verhuur (FirstName, LastName, PhoneNumber, Email, Password, BootID, DagID, DateVerhuur, LuxeID) VALUES ('$vnaam','$anaam', '$telnummer', '$email', '$ww', '$bootid', '$dagdeelid', '$datum', '$luxe')");
         header('location: bestellingen.php');
     }
 
@@ -37,13 +37,13 @@
         $datum = $_POST['datum'];
         $luxe = $_POST['luxeSelect'];
         $id = $_POST['id'];
-        db_insertData("UPDATE Verhuur SET FirstName='$vnaam', LastName='$anaam', TelephoneNumber='$telnummer', Email='$email', Password='$ww', BootID='$bootid', DagID='$dagdeelid', DateVerhuur='$datum', LuxeID='$luxe' WHERE id=$id");
+        db_insertData("UPDATE verhuur SET FirstName='$vnaam', LastName='$anaam', PhoneNumber='$telnummer', Email='$email', Password='$ww', BootID='$bootid', DagID='$dagdeelid', DateVerhuur='$datum', LuxeID='$luxe' WHERE id=$id");
         header('location: bestellingen.php');
     }
 
     if (isset($_GET['delbestelling'])) {
         $id = $_GET['delbestelling'];
-        db_insertData("DELETE FROM orders WHERE id=$id");
+        db_insertData("DELETE FROM verhuur WHERE id=$id");
         header('location: bestellingen.php');
     }
 ?>
